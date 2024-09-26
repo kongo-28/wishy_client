8b7af399b5aac7e55bbd4933c8cd66144949a0f1
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-26 19:18:17 +0900

    トップページの見た目を変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,13 @@ import { GetServerSideProps } from "next";
 import { withAuthServerSideProps } from "@/lib/auth";
 import ResponsiveAppBar from "@/components/ResponsiveAppBar";
 import styles from "@/styles/Home.module.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import CardHeader from "@mui/material/CardHeader";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
 
 export const getServerSideProps: GetServerSideProps =
   withAuthServerSideProps("/wishes");
@@ -26,14 +33,27 @@ const Home = (props: any) => {
       <div>
         <Link href="http://localhost:3001/signup">signup</Link>
       </div>
-      <div>--------------------------------------------</div>
       <div>
         {props.wishes.map((wish: any) => (
           <div key={wish.id}>
-            <p>{wish.title}</p>
-            <p>{wish.content}</p>
-            <button>HOT!</button>
-            <div>---------------------------------------</div>
+            <Card variant="outlined">
+              <CardHeader
+                title={wish.title}
+                subheader={wish.updated_at.slice(0, 16)}
+              />
+              <CardContent>
+                {" "}
+                <p>{wish.content}</p>
+              </CardContent>
+              <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                  <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                  <ShareIcon />
+                </IconButton>
+              </CardActions>
+            </Card>
           </div>
         ))}
       </div>
