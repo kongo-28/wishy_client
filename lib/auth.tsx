f8724f3ccd0b84d432fcffaf960188393e231228
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-26 18:01:54 +0900

    withAuthServerSidePropsを定義
    認証をもちつつ、axiosをたたくときに用いる

---
+++ lib/auth.tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+
+export const withAuthServerSideProps = (url: string): GetServerSideProps => {
+  return async (context) => {
+    const { req, res } = context;
+
+    const uid = req.cookies?.["uid"] || "";
+    const client = req.cookies?.["client"] || "";
+    const accessToken = req.cookies?.["access-token"] || "";
+
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      uid: uid,
+      client: client,
+      "access-token": accessToken,
+    });
+
+    const response = await fetch(`http://localhost:3000${url}`, {
+      headers: headers,
+    });
+    if (!response.ok && response.status === 401) {
+      return {
+        redirect: {
+          destination: "/sign-in",
+          permanent: false,
+        },
+      };
+    }
+    // TODO: 他にも500エラーを考慮した分岐も必要
+    const props = await response.json();
+    return { props };
+  };
+};
