545cf636f43083548ef6e936eadffc39505e3d8c
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-26 16:02:43 +0900

    signinとsignupページを作成

---
+++ pages/signin.tsx
@@ -0,0 +1,11 @@
+import SignIn from "@/components/sign-in/SignIn";
+
+const signIn = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default signIn;
---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+import SignUp from "@/components/sign-in/SignUp";
+
+const signUp = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default signUp;
