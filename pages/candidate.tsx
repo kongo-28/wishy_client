39ae7672e286ec060cdae4cc4aa7c95707735e38
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-03 18:22:41 +0900

    AIでWish候補を作成し、新たなWishを作成できるページを作成

---
+++ pages/candidate.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { GetServerSideProps } from "next";
+import { withAuthServerSideProps } from "@/lib/auth";
+import ResponsiveAppBar from "@/components/ResponsiveAppBar";
+import styles from "@/styles/Home.module.css";
+import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
+import { Paper } from "@mui/material";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+import NewWish from "@/components/NewWish";
+
+export const getServerSideProps: GetServerSideProps =
+  withAuthServerSideProps("/users/action");
+
+const Candidate = (props: any) => {
+  const [open, setOpen] = useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={styles.main}>
+      <ResponsiveAppBar domain={props.domain} />
+
+      <NewWish open={open} handleClose={handleClose} props={props} />
+      <Box
+        position="fixed"
+        bottom="20px"
+        right="20px"
+        sx={{ "& > :not(style)": { m: 1 } }}
+      >
+        <Fab onClick={handleClickOpen} color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Box>
+
+      <div className={styles.wishcontainer}>
+        <Container maxWidth="md">
+          <Paper>
+            <Card variant="outlined" sx={{ minWidth: 300 }}>
+              <div className={styles.postCard}>{props.action_plan}</div>
+            </Card>
+          </Paper>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Candidate;
