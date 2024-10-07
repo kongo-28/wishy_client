42b7a34c6cde71d7006a9b9dfe89e5257dd25592
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-07 11:03:41 +0900

    indexにプロフィールページを統合。タブで選択可能に。

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,19 @@ import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import NewWish from "@/components/NewWish";
 import Footer from "@/components/Footer";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import SendIcon from "@mui/icons-material/Send";
+import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import { red } from "@mui/material/colors";
+import { Paper } from "@mui/material";
 
 export const getServerSideProps: GetServerSideProps =
   withAuthServerSideProps("/wishes");
@@ -22,13 +35,46 @@ const Home = (props: any) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
+
+  function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      </div>
+    );
+  }
+
+  function a11yProps(index: number) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <div className={styles.main}>
       <ResponsiveAppBar domain={props.domain} />
-      {props.user && (
-        // ユーザーがいる場合に表示させるコンポーネント
-        <div>{props.user.email}でログイン中</div>
-      )}
+
       {!props.user && (
         // ユーザーがいない場合に表示させるコンポーネント
         <div>
@@ -59,11 +105,78 @@ const Home = (props: any) => {
       </Box>
 
       <div className={styles.wishcontainer}>
-        {props.wishes.map((wish: any) => (
-          <div key={wish.id} className={styles.postCard}>
-            <Wish wish={wish} user={props.user} domain={props.domain}></Wish>
-          </div>
-        ))}
+        <Container maxWidth="md">
+          <Paper>
+            <Card variant="outlined" sx={{ minWidth: 300 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    {props.user.email.slice(0, 1)}
+                  </Avatar>
+                }
+                title={props.user.email}
+              />
+              <CardContent>
+                {" "}
+                叶えたいこととかやりたいことの方針的なこと
+              </CardContent>
+              <CardActions disableSpacing>
+                総いいね:777 総WISH:77 叶えたWISH:7
+              </CardActions>
+            </Card>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained">
+                <Link href="/candidate">wish候補提案</Link>
+              </Button>
+              <Button variant="contained" onClick={handleClickOpen}>
+                アクションプラン作成
+              </Button>
+              <Button variant="contained" endIcon={<SendIcon />}>
+                wishリストの共有
+              </Button>
+            </Stack>
+          </Paper>
+        </Container>
+      </div>
+      <div className={styles.wishcontainer}>
+        <Box sx={{ width: "100%" }}>
+          <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              aria-label="basic tabs example"
+            >
+              <Tab label="ALL WISH" {...a11yProps(0)} />
+              <Tab label="MY WISH" {...a11yProps(1)} />
+              <Tab label="Coming soon..." {...a11yProps(2)} />
+            </Tabs>
+          </Box>
+          <CustomTabPanel value={value} index={0}>
+            {props.wishes.map((wish: any) => (
+              <div key={wish.id} className={styles.postCard}>
+                <Wish
+                  wish={wish}
+                  user={props.user}
+                  domain={props.domain}
+                ></Wish>
+              </div>
+            ))}
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            Item Two
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={2}>
+            {props.wishes.map((wish: any) => (
+              <div key={wish.id} className={styles.postCard}>
+                <Wish
+                  wish={wish}
+                  user={props.user}
+                  domain={props.domain}
+                ></Wish>
+              </div>
+            ))}
+          </CustomTabPanel>
+        </Box>
       </div>
 
       <Footer></Footer>
