b41db97203ae1f8360dd053fe25776aa2d39522c
kongo-28 <ryota.tennis.ku@gmail.com>
2024-10-23 07:28:11 +0900

    ダイヤログ表示時にレイアウトが崩れる問題を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,13 +67,7 @@ const Home = (props: any) => {
     <div>
       <ResponsiveAppBar domain={props.domain} />
       {loading && (
-        <Box
-          position="fixed"
-          top="50%"
-          margin="auto"
-          left="50%"
-          sx={{ display: "flex" }}
-        >
+        <Box position="fixed" top="50%" left="50%" sx={{ display: "flex" }}>
           {/* ローディング中の表示 */}
           <Box position="relative" left="-20px">
             <CircularProgress />
